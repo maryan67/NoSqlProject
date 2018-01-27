11f8bb37052986f918d68aafc596d4cfe49d94a9
Marian Secarea <secareamarian@hotmail.com>
2018-01-27 19:41:53 +0200

    Added config for hibernate

--- src/main/java/com/models/User.java
+++ src/main/java/com/models/User.java
@@ -2,4 +2,18 @@ package com.models;
 
 public class User {
 
+	
+	private int ID;
+	private String userName;
+	private String passWord;
+	private String eMail;
+	public User(int iD, String userName, String passWord, String eMail) {
+		ID = iD;
+		this.userName = userName;
+		this.passWord = passWord;
+		this.eMail = eMail;
+	}
+	
+	
+	
 }
