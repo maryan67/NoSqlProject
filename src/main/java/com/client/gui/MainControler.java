e54f75bd5b6921a86853d71b8a244361ac0504be
Virgil Meret <virgilmeret@gmail.com>
2018-01-28 00:02:21 +0200

    Login screen created.

---
+++ src/main/java/com/client/gui/MainControler.java
@@ -0,0 +1,17 @@
+package com.client.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class MainControler {
+
+	@FXML
+	private TextField txtUsername;
+	
+	@FXML
+	private PasswordField txtPassword;
+	
+	
+}
--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -6,14 +6,35 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
 import com.models.entities.*;
-public class Main {
+import com.client.gui.*;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
 
 	private static SessionFactory factory;
 	private static final Logger logger = LogManager.getLogger(Main.class.getName());
 
+	@Override
+	public void start (Stage primaryStage) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/com/client/gui/Login.fxml"));
+		Scene scene = new Scene (root);
+		primaryStage.setTitle("Login");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
 	public static void main(String[] args) {
 
+		launch(args);
+		
 		try {
 			factory = new Configuration().configure().addAnnotatedClass(User.class)
 					.addAnnotatedClass(Adress.class).addAnnotatedClass(Friendship.class)
@@ -22,6 +43,9 @@ public class Main {
 			e.printStackTrace();
 		}
 
+		
 	}
+	
+	
 
 }
